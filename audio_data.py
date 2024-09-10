ddcdae8efa7ee25b412c6e6515cf741fc577fe77
Saskia Katharina Sack <saskia@Saskias-Air.fritz.box>
2024-09-10 20:12:50 +0200

    Saskia: added main, audio_data with Xiaoman, added sms reply with song and cleaned file

---
+++ audio_data.py
@@ -0,0 +1,29 @@
+
+music_dictionary = {
+    "happy": [
+        {
+            "title": "Happy",
+            "artist": "Pharrell Williams"
+        }
+    ],
+    "stressed": [
+        {
+            "title": "The Lazy Song",
+            "artist": "Bruno Mars"
+        },
+        {
+            "title": "Business Time",
+            "artist": "Flight of the Conchords"
+        },
+        {
+            "title": "Eat It",
+            "artist": "Weird Al Yankovic"
+        }
+    ],
+    "sleepy": [
+        {
+            "title": "",
+            "artist": ""
+        }
+    ]
+}
--- messages_data.py
+++ messages_data.py
@@ -1,66 +1,63 @@
-# import API_implementation
-
+import audio_data
 import requests
 import json
+import random
 
-### SMS API
+#### SMS API
+## DATA SMS API
 
-url_sending_sms = "http://hackathons.masterschool.com:3030/sms/send"
-url_get_messages = "http://hackathons.masterschool.com:3030/team/getMessages/TooneyTunes"
-
-# DATA SMS API
 headers = {
-        'Content-Type': 'application/json'
-    }
-body = {
-  "apiToken": "ff5545473ee5ccf545e77dd52ed2a62e-6e416a63-6fa1-414a-80b6-d784479d61c8",
-  "baseUrl": "https://v36qlv.api.infobip.com",
-  "phoneNumber": 491788580904,
-  "message": "Hello, this is a test message from your terminal!"
-  #"sender": "TooneyTunes" # commented out to be able to receive messages from user
+    'Content-Type': 'application/json'
 }
-
-# create a function to enter the current phone number:
+body = {
+    "apiToken": "ff5545473ee5ccf545e77dd52ed2a62e-6e416a63-6fa1-414a-80b6-d784479d61c8",
+    "baseUrl": "https://v36qlv.api.infobip.com",
+    "phoneNumber": 491788580904,
+    "message": "Hello, this is a test message from your terminal!"
+    #"sender": "TooneyTunes" # commented out to be able to receive messages from user
+}
 
 body_requesting_emotion_reply = {
-  "apiToken": "ff5545473ee5ccf545e77dd52ed2a62e-6e416a63-6fa1-414a-80b6-d784479d61c8",
-  "baseUrl": "https://v36qlv.api.infobip.com",
-  "phoneNumber": 491788580904,
-  "message": "Hello wonderful creature, please reply to this SMS with your current mood: relaxed? moody? hyped? -> and enjoy!"
-  #"sender": "TooneyTunes" # commented out to be able to receive messages from user
+    "apiToken": "ff5545473ee5ccf545e77dd52ed2a62e-6e416a63-6fa1-414a-80b6-d784479d61c8",
+    "baseUrl": "https://v36qlv.api.infobip.com",
+    "phoneNumber": 491788580904,
+    "message": "Hello wonderful creature, please reply to this SMS with your current mood: happy? stressed? sleepy?"
+    #"sender": "TooneyTunes" # commented out to be able to receive messages from user
 }
-
-"""
 #used in def song_reply():
 song_reply = {
-  "apiToken": "ff5545473ee5ccf545e77dd52ed2a62e-6e416a63-6fa1-414a-80b6-d784479d61c8",
-  "baseUrl": "https://v36qlv.api.infobip.com",
-  "phoneNumber": 491788580904,
-  "message": "Thank you for sharing your mood - here is your Song for today: ",
-  "sender": "TooneyTunes" # commented out to be able to receive messages from user
+    "apiToken": "ff5545473ee5ccf545e77dd52ed2a62e-6e416a63-6fa1-414a-80b6-d784479d61c8",
+    "baseUrl": "https://v36qlv.api.infobip.com",
+    "phoneNumber": 491788580904,
+    "message": "Thank you for sharing your mood - here is your Song for today: {song_according_to_mood}",
+    "sender": "TooneyTunes"
 }
-"""
-
-# data requests:
-
-# commented out to safe free sms sends as we only have limited free ones
-# and I recommend to keep those for when we need to use them for testing:
-    #send_sms = requests.post(url_sending_sms, headers=headers, data=json.dumps(body))
-#send_sms = requests.post(url_sending_sms, headers=headers, data=json.dumps(body_requesting_emotion_reply))
-
-get_messages_info = requests.get(url_get_messages)
-#print(get_messages_info.text)
-#print(type(get_messages_info))
 
 
-# Testing to retrieve text messages from a specific phone number:
-def get_user_reply_sms_list_sas():
+## data requests:
+#send_sms = requests.post(url_sending_sms, headers=headers, data=json.dumps(body))
+
+def sending_message():
+    url_sending_sms = "http://hackathons.masterschool.com:3030/sms/send"
+    url_get_messages = "http://hackathons.masterschool.com:3030/team/getMessages/TooneyTunes"
+    send_sms_asking_for_mood = requests.post(url_sending_sms, headers=headers,
+                                             data=json.dumps(body_requesting_emotion_reply))
+    get_messages_info = requests.get(url_get_messages)
+
+
+# Retrieve text messages from a specific phone number:
+def send_and_get_user_reply_sms_list_sas():
     """
     Reads out the text messages from the SMS API - for 491788580904
     Converts <class 'requests.models.Response'> to json format.
     Creates List of user replies per sms.
     :return: text_replies_sms_user
     """
+    url_sending_sms = "http://hackathons.masterschool.com:3030/sms/send"
+    url_get_messages = "http://hackathons.masterschool.com:3030/team/getMessages/TooneyTunes"
+    send_sms_asking_for_mood = requests.post(url_sending_sms, headers=headers,
+                                             data=json.dumps(body_requesting_emotion_reply))
+    get_messages_info = requests.get(url_get_messages)
     get_messages_info_json = get_messages_info.json()
     # print(get_messages_info_json)
     # print(type(get_messages_info_json[0]))
@@ -73,54 +70,57 @@ def get_user_reply_sms_list_sas():
 
     return text_replies_sms_user_sas
 
-print(get_user_reply_sms_list_sas())
 
-text_replies_sms_user_sas = get_user_reply_sms_list_sas()
-
-
-def mood_jukebox():
+def mood_jukebox(list_emotion_input):
     """
     Uses the List with returned user SMS to return information about mood.
     :return: current_mood
     """
-    list_emotion_input = get_user_reply_sms_list_sas()
-
-    # latest_emotion_user_input
+    list_emotion_input = send_and_get_user_reply_sms_list_sas()
     latest_user_input_mood = list_emotion_input[-1]
     # print(latest_user_input_mood)
     current_mood = ""
-    if latest_user_input_mood == "relaxed":
-        current_mood = "relaxed"
-        print("Send relaxed song back")
-    elif latest_user_input_mood == "moody":
-        current_mood = "moody"
-        print("Send happy song")
-    elif latest_user_input_mood == "hyped":
-        current_mood = "hyped"
-        print("Send party song")
+    if latest_user_input_mood == "happy":
+        current_mood = "happy"
+    elif latest_user_input_mood == "stressed":
+        current_mood = "stressed"
+    elif latest_user_input_mood == "sleepy":
+        current_mood = "sleepy"
     else:
-        current_mood = "Nichts funktioniert, error!"
+        current_mood = "Error!"
 
     return current_mood
 
 
-print(mood_jukebox())
+#print(mood_jukebox())
 
-def music_jukebox():
+
+def music_jukebox(current_mood):
     """
     Calls mood_jukebox() -> to find the according Song
-    :return: song_according_to_mood
+    :return: song_options_according_to_mood
     """
-    mood_to_use = mood_jukebox()
+    # mood_to_use = "sleepy" - testing leftover
 
-    # return song_according_to_mood
+    mood_to_use = current_mood
+    song_options_according_to_mood = []
+    for mood, song_collection in audio_data.music_dictionary.items():
+        if mood_to_use == mood:
+            for song_details in song_collection:
+                song_options_according_to_mood.append((song_details["artist"], song_details["title"]))
 
-def song_reply(song_according_to_mood):
+    return song_options_according_to_mood
+
+
+def song_reply(song_options_according_to_mood):
     """
     uses music_jukebox() --> to send SMS back
     :return:
     """
-    # song_according_to_mood - implement somehow in message
+    # randomize over tuples:
+    song_tuple_choice = song_options_according_to_mood.pop(random.randrange(len(song_options_according_to_mood)))
+    # adding the scraped song:
+
     song_reply = {
         "apiToken": "ff5545473ee5ccf545e77dd52ed2a62e-6e416a63-6fa1-414a-80b6-d784479d61c8",
         "baseUrl": "https://v36qlv.api.infobip.com",
@@ -128,10 +128,17 @@ def song_reply(song_according_to_mood):
         "message": "Thank you for sharing your mood - here is your Song for today: {song_according_to_mood}",
         "sender": "TooneyTunes"
     }
-    send_sms = requests.post(url_sending_sms, headers=headers, data=json.dumps(song_reply))
+    song_tuple_choice = song_options_according_to_mood.pop(random.randrange(len(song_options_according_to_mood)))
+    #one could still unpack the tuple and present it in a nicer way in the message
+    song_reply[
+        "message"] = "Thank you for sharing your mood - here is your Song for today: " + f"{song_tuple_choice} " + "!!!"
+
+    # sending the final message
+    url_sending_sms = "http://hackathons.masterschool.com:3030/sms/send"
+    send_sms_song_reply = requests.post(url_sending_sms, headers=headers, data=json.dumps(song_reply))
 
 
-###########################
+##################################################################
 # in case we figure out a better way, this could be useful:
 
 def get_all_user_replies_sms_list():
@@ -149,4 +156,17 @@ def get_all_user_replies_sms_list():
 
     return list_all_text_replies_sms_user
 
-# print(get_all_user_replies_sms_list())
+
+#print(get_all_user_replies_sms_list())
+
+# second version for extracting a random song:
+def get_random_song(current_mood):
+    music_data = audio_data.music_dictionary
+    if current_mood in music_data:
+        random_song = random.choice(audio_data.music_dictionary[current_mood])
+        print(f"Title: {random_song['title']}, Artist: {random_song['artist']}")
+    else:
+        print("Mood not found")
+
+
+# get_random_song("sleepy")
